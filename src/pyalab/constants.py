9898c8010fa5e7d26a89371f10595a9c63a2650b
Eli Fine <ejfine@gmail.com>
2024-12-17 18:26:09 +0000

    moved files

--- src/pyalab/constants.py
+++ src/pyalab/constants.py
@@ -4,4 +4,4 @@ PATH_TO_PACKAGE_ROOT = Path(__file__).absolute().resolve().parent
 assert str(PATH_TO_PACKAGE_ROOT).endswith(
     "pyalab"
 ), f"Sanity check failed, path was not to package root, it was: {PATH_TO_PACKAGE_ROOT}"
-PATH_TO_INCLUDED_XML_FILES = PATH_TO_PACKAGE_ROOT / "integra_library"
+PATH_TO_INCLUDED_XML_FILES = PATH_TO_PACKAGE_ROOT / "vendor_files" / "integra_library"
--- src/pyalab/integra_xml.py
+++ src/pyalab/integra_xml.py
@@ -1,9 +1,9 @@
 import re
 from enum import Enum
 from pathlib import Path
-from xml.etree.ElementTree import Element
 
 from lxml import etree
+from lxml.etree import _Element
 from pydantic import BaseModel
 
 from .constants import PATH_TO_INCLUDED_XML_FILES
@@ -25,7 +25,7 @@ class LibraryComponent(BaseModel):
     type: LibraryComponentType
     name: str
     xml_file_version: str | None = None
-    _xml_root: Element | None = None
+    _xml_root: _Element | None = None
 
     def load_xml(self) -> None:
         directory = PATH_TO_INCLUDED_XML_FILES / self.type.value
@@ -42,10 +42,10 @@ class LibraryComponent(BaseModel):
         parser = etree.XMLParser(no_network=True, recover=False)
         tree = etree.parse(file, parser)  # noqa: S320 # using this custom parser should mitigate security concerns about untrusted XML files. And defusedxml is end of life anyway
         root = tree.getroot()
-        assert isinstance(root, Element), f"Expected root to be an Element, but got type {type(root)} for {root}"
+        assert isinstance(root, _Element), f"Expected root to be an Element, but got type {type(root)} for {root}"
         self._xml_root = root
 
     @property
-    def xml_root(self) -> Element:
-        assert isinstance(self._xml_root, Element)
+    def xml_root(self) -> _Element:
+        assert isinstance(self._xml_root, _Element)
         return self._xml_root
