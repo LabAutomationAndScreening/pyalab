4807f1f91bc934be140ba795006aef0281c8ca2b
Eli Fine <ejfine@gmail.com>
2024-12-17 14:35:34 +0000

    template update

--- .devcontainer/initialize-command.sh
+++ .devcontainer/initialize-command.sh
@@ -1,6 +1,7 @@
 # CodeSpaces build environments get cluttered and need to be pruned https://github.com/orgs/community/discussions/50403
 # There was an error encountered during the codespace build using `docker system prune`, so switched to `docker image prune`: Error: The expected container does not exist.
-# This was made executable using chmod +x initialize-command.sh
+set -ex
+
 printenv
 if [ -n "$CODESPACES" ] && [ "$CODESPACES" = "true" ]; then
     docker image prune --all --force
--- .devcontainer/on-create-command-boilerplate.sh
+++ .devcontainer/on-create-command-boilerplate.sh
@@ -4,5 +4,6 @@ set -ex
 sh .devcontainer/install-ci-tooling.sh
 
 git config --global --add --bool push.autoSetupRemote true
+git config --local core.symlinks true
 
 sh .devcontainer/create-aws-profile.sh
