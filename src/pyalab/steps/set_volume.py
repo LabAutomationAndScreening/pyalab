3020d552349f0be6b1d270832772ab6a716d1bc9
Eli Fine <eli88fine@gmail.com>
2024-12-18 20:35:17 -0500

    Additional step documentation (#11)
    ## Why is this change necessary?
    Not all steps were documented in Sphinx. Also, readthedocs was failing
    because dependencies weren't installed.
    
    
     ## How does this change address the issue?
    Fixes readthedocs config to actually install the requirements. Documents
    the set volume steps.
    
    
     ## What side effects does this change have?
    None
    
    
     ## How is this change tested?
    Local doc build
    
    ---------
    
    Co-authored-by: Eli Fine <ejfine@gmail.com>

--- src/pyalab/steps/set_volume.py
+++ src/pyalab/steps/set_volume.py
@@ -11,11 +11,20 @@ from .base import ul_to_xml
 
 
 class SetVolume(Step):
+    """Specify the volume of liquid in the labware.
+
+    Can be used to initially define it at the beginning of a protocol, or after a manual filling step.
+    """
+
     type = "ManualFilling"
     plate: Plate
+    """The plate to set the volume for."""
     section_index: int | None = None
+    """The section of the Deck holding the plate."""
     column_index: int
+    """The column within the plate to set the volume for."""
     volume: float = Field(ge=0)
+    """The specified volume (µl)."""
 
     @override
     def _add_value_groups(self) -> None:
@@ -50,4 +59,9 @@ class SetVolume(Step):
 
 
 class SetInitialVolume(SetVolume):
+    """Must be used as the first step in the program that set's the volume.
+
+    Uses the same parameters as SetVolume.
+    """
+
     type = "ManualFilling_First"
