3b1440716bebfc0b29bfa16f79c2534d36f8cb23
Eli Fine <eli88fine@gmail.com>
2025-09-15 11:15:33 +0000

    copier

--- .devcontainer/install-ci-tooling.py
+++ .devcontainer/install-ci-tooling.py
@@ -7,9 +7,9 @@ import sys
 import tempfile
 from pathlib import Path
 
-UV_VERSION = "0.8.15"
+UV_VERSION = "0.8.17"
 PNPM_VERSION = "10.15.1"
-COPIER_VERSION = "9.10.1"
+COPIER_VERSION = "9.10.2"
 COPIER_TEMPLATE_EXTENSIONS_VERSION = "0.3.3"
 PRE_COMMIT_VERSION = "4.3.0"
 GITHUB_WINDOWS_RUNNER_BIN_PATH = r"C:\Users\runneradmin\.local\bin"
